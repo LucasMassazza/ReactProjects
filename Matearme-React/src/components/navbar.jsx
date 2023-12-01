c3bc394de7795abe8c57b901e33332acc13fffea
Lucas Massazza <lucasmassazza@gmail.com>
2023-11-30 23:40:52 -0300

    0.0.1
    Inicio

---
+++ Matearme-React/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+
+export const Navbar = () => {
+    return (
+
+        <header>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Mates</a></li>
+            </ul>
+            <img src=".\src\icons\placeholder.png" alt="" />
+            <ul>
+                <li><a href="#">Termos</a></li>
+                <li><a href="#">Bombillas</a></li>
+            </ul>
+            <img src=".\src\icons\placeholder.png" alt="Perfil" />
+            <img src=".\src\icons\placeholder.png" alt="Carrito" />
+        </header>
+    )
+}
